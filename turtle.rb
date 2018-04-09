478b138eb4fed3d161a0f4687469a07d3d47c971
Michael Adams <mtadams007@gmail.com>
2018-04-09 13:56:50 -0400

    fixed fun fact method to include last fact

--- turtle.rb
+++ turtle.rb
@@ -17,7 +17,7 @@ class Turtle < Reptile
   def self.fun_fact
     fun_facts = ["Turtles also have a lower shell called a ‘plastron’.", "Turtles have existed for around 215 million years", "The largest turtle is the leatherback sea turtle, it can weigh over 900 kg! (2000 lb)", "Many turtle species are endangered.", "Sea turtles have special glands which help remove salt from the water they drink."]
     length = fun_facts.length
-    fun_facts[rand(length-1)]
+    fun_facts[rand(length)]
   end
 end
 
